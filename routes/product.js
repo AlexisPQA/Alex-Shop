0858224731214f8969c9cf89043a4b08ffe8e8ee
AlexisPQA <ppaax1@gmail.com>
2021-01-09 02:00:47 +0700

    add controllers for product

--- routes/product.js
+++ routes/product.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+const product = require('../controllers/Product.controller'); 
 /* GET product page. */
 router.get('/', function(req, res, next) {
   res.render('products');
   
 });
+router.get('/', book.index);
 
 module.exports = router;
