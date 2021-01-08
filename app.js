0e78a64b93f481abf8580fd512f8f23a67988922
AlexisPQA <ppaax1@gmail.com>
2021-01-09 01:30:14 +0700

    add index

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
+require('dotenv').config()
+
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongoose = require('mongoose')
+var hbs = require('express-handlebars')
+const hbshelper = require('handlebars-helpers')
+const multihelpers = hbshelper()
+
+mongoose.connect(process.env.url, {useNewUrlParser: true, useUnifiedTopology: true})
+
+
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
+app.engine("hbs", hbs({
+	extname: "hbs",
+	helpers : multihelpers,
+	defaultLayout:false,
+	layoutsDir : __dirname + "/views/layout/",
+	paritalsDir: __dirname + "/views/paritals/",
+	runtimeOptions:{
+		allowProtoPropertiesByDefault: true,
+		allowProtoMethodsByDefault:true,
+	},
+}))
+
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,7 +41,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/index', indexRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
