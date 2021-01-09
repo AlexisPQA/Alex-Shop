776415cc82b5c40d6c002b9592872a19e357c3bc
AlexisPQA <ppaax1@gmail.com>
2021-01-09 12:15:39 +0700

    get(:/id) product

--- routes/product.js
+++ routes/product.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 const product = require('../controllers/Product.controller'); 
 /* GET product page. */
-router.get('/', function(req, res, next) {
-  res.render('products');
-  
-});
-router.get('/', book.index);
-
+router.get('/', product.index);
+router.get('/:id', product.detail);
 module.exports = router;
