a15a4cead8fb07c17bb8f5b76ee1d3c4bd6b163c
AlexisPQA <ppaax1@gmail.com>
2021-01-09 13:01:36 +0700

    added pagination

--- controllers/Product.controller.js
+++ controllers/Product.controller.js
@@ -1,8 +1,27 @@
 const books = require('../models/book.model')
+ItemPerPage = 9
+// exports.index = (req, res, next) => {
+//     books.find().then(function(book){
+// 		res.render('products', {book:book});
+// 	})
+// };
 
-exports.index = (req, res, next) => {
-    books.find().then(function(book){
-		res.render('products', {book:book});
+exports.index = async (req, res, next) => {
+	const page = +req.query.page || 1;
+	const paginate = await books.paginate({},{
+		page:page,
+		limit:ItemPerPage,
+	});
+	console.log(paginate)
+	res.render('products',{
+		book: paginate.docs,
+		currentPage : paginate.page,
+		hasNextPage : paginate.hasNextPage,
+		hasPreviousPage : paginate.hasPrevPage,
+		nextPage : paginate.nextPage,
+		prevPage : paginate.prevPage,
+		lastPage : paginate.totalPages,
+		ITEM_PER_PAGE: ItemPerPage,
 	})
 };
 
