9f19df4dbd1c67d32b16044c413ef23d6a3aafa4
AlexisPQA <ppaax1@gmail.com>
2021-01-09 12:14:58 +0700

    book model

---
+++ models/book.model.js
@@ -0,0 +1,15 @@
+var moongse = require('mongoose')
+const mongoosePaginate = require('mongoose-paginate-v2');
+var bookSchema = new moongse.Schema({
+    title: String,
+    cover: String,
+    detail: String,
+    basePrice: String,
+    category:String,
+    author:String
+});
+
+bookSchema.plugin(mongoosePaginate)
+var Books = moongse.model('book',bookSchema,'books')
+module.exports = Books
+
